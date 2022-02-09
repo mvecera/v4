895ce7dcbd49cf08f05b1531ae1d288112928893
mvecera <mvrtin.vecera@gmail.com>
2022-02-09 18:09:13 +0100

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,13 +59,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Martin Vecera.</h2>;
+  const three = <h3 className="big-heading">I build the websites.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a web developer specializing in CMS platforms. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
